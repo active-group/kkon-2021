cbb003b6bcd030c52edd84b1a028b61b4b3baf1e
Mike Sperber <sperber@deinprogramm.de>
2021-09-13 10:26:11 +0200

    Update.

---
+++ src/main/kotlin/Animals.kt
@@ -0,0 +1,12 @@
+// Tiere auf dem texanischen Highway
+// Da gibt es Gürteltiere.
+// Wollen erfassen: tot oder lebendig? Gewicht?
+// Information -> Daten
+// DeinProgramm: Konstruktionsanleitungen
+// Datendefinition:
+// Ein Gürteltier hat folgende Eigenschaften:
+//                ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//                (alternative Formulierung: besteht aus)
+//                zusammengesetzte Daten (hier nicht: Komposition)
+// - tot oder lebendig?
+// - Gewicht
