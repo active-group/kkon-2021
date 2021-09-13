c959d264560b41e98abba4a6d597615a596a0ba9
Mike Sperber <sperber@deinprogramm.de>
2021-09-13 11:59:27 +0200

    Update.

--- src/main/kotlin/Shape.kt
+++ src/main/kotlin/Shape.kt
@@ -7,7 +7,19 @@
 //                            ^^^^^^^^^^^^^^^^^^^^^
 //                            Selbstbezug -> rekursiver Aufruf
 
+
 // 1. Schreibe eine Repräsentation für geometrische Figuren
 // 2. Schreibe eine Funktion/Methode, die für einen Punkt
 //    aussagt, ob dieser innerhalb oder außerhalb der
 //    geometrischen Figur ist
+
+data class Point(val x: Double, val y: Double)
+
+sealed interface Shape {
+    fun contains(point: Point): Boolean
+}
+data class Overlay(val shape1: Shape, val shape2: Shape): Shape {
+    override fun contains(point: Point): Boolean =
+        this.shape1.contains(point) || // enthält shape1 den point?
+        this.shape2.contains(point) // enthält shape2 den point?
+}
