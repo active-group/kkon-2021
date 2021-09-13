bfbefd336e2873c4619438d621a5449023f15b6f
Mike Sperber <sperber@deinprogramm.de>
2021-09-13 12:24:41 +0200

    Update.

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -2,8 +2,18 @@
 // - die leere Liste
 // - eine Cons-Liste bestehend aus erstem Element und Rest-Liste
 //                                                         ^^^^^
-sealed interface List
+sealed interface List {
+    fun sum(): Int
+}
 
-object Empty : List
+object Empty : List {
+    override fun sum(): Int = 0
+}
 
-data class Cons(val first: Int, val rest: List) : List
+data class Cons(val first: Int, val rest: List) : List {
+    override fun sum(): Int =
+        this.first + this.rest.sum()
+}
+
+// 3elementige Liste: 1, 2, 3
+val list1 = Cons(1, Cons(2, Cons(3, Empty)))
